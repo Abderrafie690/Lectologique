b44b5171552ff9bdc93fbf67d34bf2d5fa5df41e
Abderrafie690 <bouejara338@hotmail.com>
2025-07-12 12:46:11 +0100

    Resliser línterface utilizateur ok

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -6,8 +6,10 @@ export const allRoutes = [
   new Route("/", "Accueil", "views/pages/home.html", ),
 
   new Route("/galerie", "Galerie", "views/pages/general/galerie.html", ""),
+ new Route("/allResa", "reservations", "views/reservations/allResa.html", ""),
+ new Route("/reserver", "réservations", "views/pages/reserver.html", ""),
   new Route("/dashboard", "Tableau de bord", "views/pages/dashboard.html", "js/auth/dashboard.js"),
-  new Route("/profile", "Profil", "views/pages/auth/profile.html", "js/auth/profile.js"),
+  new Route("/profile", "Mon compte", "views/pages/auth/profile.html", "js/auth/profile.js"),
   new Route("/edit-password", "Modifier mot de passe", "views/pages/auth/edit_password.html", "js/auth/edit_password.js"),
   new Route("/logout", "Déconnexion", "views/pages/auth/logout.html", "js/auth/logout.js"),
   new Route("/login", "Connexion", "views/pages/auth/login.html", "js/auth/login.js"),
