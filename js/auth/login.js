3d61fa77e75cf6283203c280a9bc7581e9663271
Abderrafie690 <bouejara338@hotmail.com>
2025-07-12 19:31:59 +0100

    connexion avec authentificacion ok

--- js/auth/login.js
+++ js/auth/login.js
@@ -1,32 +1,20 @@
-alert("coocoo");
+const mailInput = document.getElementById("EmailInput");
+const passwordInput = document.getElementById("PasswordInput");
+const btnSignin = document.getElementById("btnSignin");
 
 
-document.addEventListener('DOMContentLoaded', () => {
-  const form = document.querySelector('#loginForm');
 
-  form?.addEventListener('submit', async e => {
-    e.preventDefault();
+btnSignin.addEventListener("click", checkCredentials);
 
-    const formData = new FormData(form);
+function checkCredentials(){
+    if(mailInput.value == "test@mail.com" && passwordInput.value == "123"){
+        alert("vous êtes connecté");
 
-    try {
-      const response = await fetch('../login_api.php', {
-        method: 'POST',
-        body: formData
-      });
+      
 
-      const result = await response.json();
-
-      if (result.success) {
-        window.location.href = '/dashboard';
-      } else {
-        const alert = document.querySelector('#loginError');
-        alert.textContent = result.message;
-        alert.classList.remove('d-none');
-      }
-
-    } catch (err) {
-      console.error('Erreur réseau:', err);
+        window.location.replace("/"); // ✅ REDIRECCIÓN AL FINAL
+    } else {
+        mailInput.classList.add("is-invalid");
+        passwordInput.classList.add("is-invalid");
     }
-  });
-});
+}
